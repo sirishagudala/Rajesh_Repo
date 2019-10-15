105403e729090870a0c9f311de5b01691331ce74
sireesha gudala <sirisha.gudala@gmail.com>
2019-10-15 06:38:59 -0400

    Committing Files

---
+++ Rajesh_Proj_Test/src/Bank.java
@@ -0,0 +1,30 @@
+public abstract class Bank {
+	public abstract void loan();
+	public void credit()
+	{
+	System.out.println("credit card");
+	
+	System.out.println("credit card");
+        System.out.println("credit card");
+		
+	}
+	public void debit()
+	{
+	System.out.println("debit card");
+		
+	}
+	
+	
+Bank()
+{
+	
+}
+Bank(String name)//we can create constructor in abstract class.
+{
+	
+}
+/*public static void main(String[] args)
+{
+Bank obj= new Bank();//we cannot create an object for an abstract class.	
+}*/
+}
